0939225e58f34aa864834edd4e4f09f420fca5ad
Dan <yousong@pop-os.localdomain>
2023-11-09 07:28:13 +0000

    adding tester

---
+++ TESTDOME/libft/ft_strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yousong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 02:54:22 by yousong           #+#    #+#             */
+/*   Updated: 2023/11/06 16:08:16 by yousong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] && i < dstsize)
+		i++;
+	while (src[j] && i + j < dstsize - 1)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	if (i < dstsize)
+		dest[i + j] = '\0';
+	return (i + j);
+}
+
+/*int	main(void)
+{
+	char	str1[15] = "sourcesource";
+	char	str2[30] = "destdest";
+
+	printf("%s\n", str2);
+	ft_strlcat(str2, str1, 20);
+	printf("%s\n", str2);
+	return (0);
+}*/
---
+++ TESTDOME/libft/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yousong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 12:25:57 by yousong           #+#    #+#             */
+/*   Updated: 2023/11/09 07:08:14 by yousong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
+#include <string.h>*/
+#include "libft.h"
+
+size_t	ft_strlen(const char*s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/*int	main(void)
+{
+	printf("%d", ft_strlen("stringstringstring"));
+	printf("\n%zu", strlen("stringstringstring"));
+	return (0);
+}*/
