701aba05b378c1b06175aab4cd3c8a6103dc4c15
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-11-10 03:56:22 +0000

    updating functions

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -16,6 +16,8 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 		write(fd, "-2147483648", 11);
+	else if (n == 0)
+		write(fd, "0", 1);
 	else if (n < 0)
 	{
 		write(fd, "-", 1);
@@ -24,7 +26,7 @@ void	ft_putnbr_fd(int n, int fd)
 	else if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10 + '0', fd);
+		ft_putchar_fd(n % 10 + '0', fd);
 	}
 	else
 		ft_putchar_fd(n + '0', fd);
