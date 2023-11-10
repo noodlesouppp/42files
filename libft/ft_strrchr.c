82eb99a80c3c1af30c64c1dfdbce0bd64adb2b70
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-11-10 04:48:31 +0000

    updating functions

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *str, int c)
 	int	i;
 
 	i = ft_strlen(str);
-	if (c == '\0')
+	if ((unsigned char)c == '\0')
 		return ((char *)&str[i]);
 	while (i >= 0)
 	{
-		if (str[i] == c)
+		if ((unsigned char)str[i] == (unsigned char)c)
 			return ((char *)&str[i]);
 		i--;
 	}
