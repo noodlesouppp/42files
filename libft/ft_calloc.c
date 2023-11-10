1d7b5bbf2b1fceb4b1a0aea152a220767899684f
Dan <yousong@pop-os.localdomain>
2023-11-10 07:59:29 +0000

    updating functions

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: yousong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:50:54 by yousong           #+#    #+#             */
-/*   Updated: 2023/11/07 21:35:43 by yousong          ###   ########.fr       */
+/*   Updated: 2023/11/10 07:57:36 by yousong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
-	
+
 	if (count != 0 && size > ((size_t)-1) / count)
 		return (NULL);
 	ptr = malloc(count * size);
