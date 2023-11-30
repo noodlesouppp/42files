ef044d91331372b1abe79133549429c91ae0b280
Dan! <dansong315@gmail.com>
2023-11-30 06:04:58 +0000

    updating functions

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include "get_next_line.h"
