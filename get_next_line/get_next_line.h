fd8c66afe400b533fe01dccdc5b2b81f203a77a2
Dan! <dansong315@gmail.com>
2023-11-30 07:34:16 +0000

    updating header

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -2,6 +2,8 @@
 # define	GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
@@ -9,9 +11,20 @@
 
 typedef struct	s_node
 {
-	char		*content;
+	char			*content;
 	struct s_node	*next;
 }
 node;
+
+char	*get_next_line(int fd);
+void	ft_list(node **stash, int fd);
+void	ft_append(node **stash, char *buf);
+char	*ft_line(node *stash);
+void	ft_leftover(node **stash);
+int		ft_newline(node *stash);
+int		count_to_newline(node *stash);
+node 	*ft_last_node(node *stash);
+void	ft_strcpy(node *stash, char *next_line);
+void	ft_dealloc(node **stash, node *clean, char *buf);
 
 #endif
