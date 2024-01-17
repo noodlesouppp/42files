dfc4f770375f3d7b3d4bec549f1c7e3737a66d36
yousong <dansong315@gmail.com>
2024-01-17 18:57:46 +0000

    adding init files

---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int ft_printstr(char *str)
+{
+    int i;
+
+    i = 0;
+    if (str == NULL)
+    {
+        ft_putstr("(null)");
+        return (6);
+    }
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+    return (i);
+}
+
+int ft_printnbr(int n)
+{
+    
+}
